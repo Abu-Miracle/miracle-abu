c95f759831a35dbf2790c52c36d91ebc9f17b19f
Abu-Miracle <miracleabu07@gmail.com>
2025-09-10 23:11:56 +0100

    fix: formatted code

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,17 +5,31 @@ export default function Footer() {
   return (
     <div className="bg-linear-to-l from-[var(--light-gray)] from-95% to-[var(--dark-orange)] p-[2px] rounded-[14px] h-[66px] lex justify-center items-center mt-3">
       <div className="bg-linear-to-l w-full rounded-xl h-full from-[var(--dark-gray)] from-60% to-[#33130E] justify-between flex items-center md:px-6 px-4">
-        <div className="hidden md:flex text-[var(--text-light)]">Portfolio 2025</div>
+        <div className="hidden md:flex text-[var(--text-light)]">
+          Portfolio 2025
+        </div>
 
         <div className="flex space-x-4 md:space-x-6">
           <Link href="https://x.com/miracle_abu01" target="_blank">
-            <FaXTwitter size={23} className="cursor-pointer hover:text-[var(--orange)] transition-colors duration-300" />
+            <FaXTwitter
+              size={23}
+              className="cursor-pointer hover:text-[var(--orange)] transition-colors duration-300"
+            />
           </Link>
-          <Link href="https://github.com/Abu-Miracle" target="_blank" >
-            <FaGithub size={23} className="cursor-pointer hover:text-[var(--orange)] transition-colors duration-300" />
+          <Link href="https://github.com/Abu-Miracle" target="_blank">
+            <FaGithub
+              size={23}
+              className="cursor-pointer hover:text-[var(--orange)] transition-colors duration-300"
+            />
           </Link>
-          <Link href="https://www.linkedin.com/in/miracle-abu-2018412b1/" target="_blank">
-            <FaLinkedin size={23} className="cursor-pointer hover:text-[var(--orange)] transition-colors duration-300" />
+          <Link
+            href="https://www.linkedin.com/in/miracle-abu-2018412b1/"
+            target="_blank"
+          >
+            <FaLinkedin
+              size={23}
+              className="cursor-pointer hover:text-[var(--orange)] transition-colors duration-300"
+            />
           </Link>
         </div>
 
