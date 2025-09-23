6e872e148feb53609bcdd72b2ad518109f84a554
Abu-Miracle <miracleabu07@gmail.com>
2025-09-23 15:00:38 +0100

    feat: open graph configuration for preview link

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,28 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Miracle Abu",
   description: "A portfolio web application for Miracle Abu",
+  openGraph: {
+    title: "Miracle Abu",
+    description: "A portfolio web application for Miracle Abu",
+    url: "https://miracle-abu01.vercel.app/",
+    siteName: "Miracle Abu Portfolio",
+    images: [
+      {
+        url: "/portfolio_thumbnail.png", 
+        width: 1200,
+        height: 630,
+        alt: "Miracle Abu Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Miracle Abu",
+    description: "A portfolio web application for Miracle Abu",
+    images: ["https://miracle-abu01.vercel.app/portfolio_thumbnail.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
